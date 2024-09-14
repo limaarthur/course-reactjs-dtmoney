8d74fb56ae5681b02e5844bc4952cd5c1d2b57cf
Arthur Lima <artglima.dev@gmail.com>
2024-09-14 11:15:34 -0300

    visual structure - Summary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./themes/default";
 import { Header } from "./components/Header";
+import { Summary } from "./components/Summary";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Header />
+      <Summary />
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+import totalImg from "../../assets/total.svg"
+
+import { SummaryContainer, SummaryCard } from "./styles"
+
+export function Summary() {
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>R$ 17.400,00</strong>
+            </SummaryCard>
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>R$ 1.400,00</strong>
+            </SummaryCard>
+            <SummaryCard className="highlight-background">
+                <header>
+                    <span>Total</span>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$ 16.000,00</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
