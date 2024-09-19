bac0ab5ebd56015e8e744f426c08d109e7ac4f61
Arthur Lima <artglima.dev@gmail.com>
2024-09-19 15:46:33 -0300

    visual structure - accessible modal created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react"
+import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg"
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="dt money" />
-                <NewTransactionButton>
+                <NewTransactionButton onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </NewTransactionButton>
+                <Modal 
+                    isOpen={isNewTransactionModalOpen} 
+                    onRequestClose={handleCloseNewTransactionModal}
+                >
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </HeaderContent>
         </HeaderContainer>
     )
